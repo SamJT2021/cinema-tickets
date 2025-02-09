b375691c17ca34b4b7648511ff41c72b73e33ebd
SamJT2021 <samuel.j.thoburn@outlook.com>
2025-02-09 14:45:48 +0000

    chore(project-structure): move files to parent dir for easier management, add pre-commit hook

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -5,8 +5,7 @@ export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
-
   // purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
+  // throws InvalidPurchaseException
   // }
 }
