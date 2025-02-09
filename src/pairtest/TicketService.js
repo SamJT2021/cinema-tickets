213f3fd801eeb249d2869d5ca133a3b2b760acf8
SamJT2021 <samuel.j.thoburn@outlook.com>
2025-02-09 15:43:32 +0000

    chore(pre-commit): fix lint errors after pre-commit test

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,12 +1,11 @@
-import TicketTypeRequest from "./lib/TicketTypeRequest.js";
-import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
+// import TicketTypeRequest from "./lib/TicketTypeRequest.js";
+// import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
-
-  purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
-  }
+  // purchaseTickets(accountId, ...ticketTypeRequests) {
+  // throws InvalidPurchaseException
+  // }
 }
